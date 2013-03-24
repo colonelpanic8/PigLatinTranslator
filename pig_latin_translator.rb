c78912d26702bd6b4021147596ae2835d1f812c0
Ivan Malison <IvanMalison@gmail.com>
2013-03-23 19:58:29 -0700

    Made stuff more object oriented.

--- pig_latin_translator.rb
+++ pig_latin_translator.rb
@@ -1,30 +1,32 @@
-class PigLatinTranslator
+class PigLatinWord
 
   @@letters = ('a' .. 'z').to_a
   @@vowels = %w(a e i o u)
   @@consonants = @@letters - @@vowels
+
+  attr_reader :word
   
-  def translate(sentence)
-    return sentence.split(' ').map {|word| translate_word(word)}.join(' ')
+  def initialize(word)
+    @word = word
   end
 
-  def translate_word(word)
+  def is_capitalized?
+    word == word.capitalize
+  end
+
+  def translate
     normalized_word = word.downcase.gsub(/[^a-z]/, '')
     pig_word_start = get_starting_pig_index(normalized_word)
     pig_latin_word = [normalized_word[pig_word_start, normalized_word.length],
                       normalized_word[0...pig_word_start],
                       'ay'].join('')
-    pig_latin_word.capitalize! if self.is_capitalized?(word)
+    pig_latin_word.capitalize! if self.is_capitalized?
     pig_latin_word += word.downcase.gsub(/[a-z]/, '')
     return pig_latin_word
   end
-  
-  def is_capitalized?(word)
-    word == word.capitalize
-  end
- 
-  def has_punctuation?(word)
-    !@@letters.include?(word.downcase[-1])
+
+  def to_s
+    return "#{self.translate}"
   end
 
   private
@@ -38,5 +40,16 @@ class PigLatinTranslator
       end
     end
     return pig_word_start
+  end
+end
+
+class PigLatinTranslator
+
+  def initialize(sentence)
+    @pig_latin_words = sentence.split(' ').map {|word| PigLatinWord.new(word)}
+  end
+  
+  def translation
+    return @pig_latin_words.join(' ')
   end
 end
--- translate.rb
+++ translate.rb
@@ -1,8 +1,5 @@
 require './pig_latin_translator.rb'
 
-translator = PigLatinTranslator.new
-
 while true
-  word_to_translate = gets.chomp
-  puts translator.translate(word_to_translate)
+  puts PigLatinTranslator.new(gets.chomp).translation
 end
